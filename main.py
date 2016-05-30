487ca691d12f59b0fe9a1747b6d9313ddaba1aa3
Mancuniancol <scsi72@yahoo.com>
2016-05-30 06:46:00 -0400

    v0.3.2 dialog copy

--- main.py
+++ main.py
@@ -43,11 +43,13 @@ elif mode == 'copy':
                 items.append(provider['addonid'])
         items.remove(addonid)
         ret = xbmcgui.Dialog().select('Select the provider', ['All providers'] + items + ['CANCEL'])
+        list_copy = (items if ret == 0 else [items[ret - 1]])
         if ret != -1 and ret <= len(items):
             for key, val in value.items():
-                for provider in (items if ret == 0 else [items[ret-1]]):
+                for provider in list_copy:
                     xbmcaddon.Addon(provider).setSetting(id=key, value=val)
-            xbmcgui.Dialog().notification('Magnetic', 'All the settings were copied')
+            xbmcgui.Dialog().ok('Magnetic',
+                                'The %s settings were copied to \n%s' % (addonid, '\n'.join(list_copy)))
 
 
 elif mode == 'check':
