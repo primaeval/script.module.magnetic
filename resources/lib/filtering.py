eda8868ec896bf40dc2359270fa63a33e352c8bc
Mancuniancol <scsi72@yahoo.com>
2016-11-12 16:22:59 -0500

    v0.8.9 activation sorting

--- resources/lib/filtering.py
+++ resources/lib/filtering.py
@@ -1,10 +1,10 @@
 import logger
-from utils import Magnet
+from utils import Magnet, get_float
 
 
 # filter results
 def apply_filters(results_list):
-    # results_list = cleanup_results(results_list)
+    results_list = cleanup_results(results_list)
     return sort_by_quality(results_list)
 
 
@@ -16,25 +16,101 @@ def cleanup_results(results_list):
 
     filtered_list = []
     for result in results_list:
-        # noinspection PyBroadException
-        try:
-            # check provider returns seeds
-            int(result['seeds'])
+        # check provider returns seeds
+        # get_int(result['seeds'])
 
-            # append hash
-            result['hash'] = Magnet(result['uri']).info_hash.upper()
+        # append hash
+        result['hash'] = Magnet(result['uri']).info_hash.upper()
+        logger.log.debug(result['hash'])
 
-            # remove dupes
-            # noinspection PyTypeChecker
-            if len([item for item in filtered_list if item['hash'].upper() == result['hash'].upper()]) == 0:
-                # append item to results
-                filtered_list.append(result)
+        # remove dupes
+        # noinspection PyTypeChecker
+        if len([item for item in filtered_list if item['hash'].upper() == result['hash'].upper()]) == 0 or len(
+                result['hash']):
+            # append item to results
+            filtered_list.append(result)
 
-        except:
-            logger.log.info("Failed to parse:" + str(result))
-            pass
+    return sorted(filtered_list, key=lambda r: (get_float(r['seeds'])), reverse=True)
 
-    return sorted(filtered_list, key=lambda r: (float(r['seeds'])), reverse=True)
+
+def check_quality(text=""):
+    # quality
+    key_words = {"Cam": ["camrip", "cam"],
+                 "Telesync": ["ts", "telesync", "pdvd"],
+                 "Workprint": ["wp", "workprint"],
+                 "Telecine": ["tc", "telecine"],
+                 "Pay-Per-View Rip": ["ppv", "ppvrip"],
+                 "Screener": ["scr", "screener", "screeener", "dvdscr", "dvdscreener", "bdscr"],
+                 "DDC": ["ddc"],
+                 "R5": ["r5", "r5.line", "r5 ac3 5 1 hq"],
+                 "DVD-Rip": ["dvdrip", "dvd-rip"],
+                 "DVD-R": ["dvdr", "dvd-full", "full-rip", "iso rip", "lossless rip", "untouched rip", "dvd-5 dvd-9"],
+                 "HDTV": ["dsr", "dsrip", "dthrip", "dvbrip", "hdtv", "pdtv", "tvrip", "hdtvrip", "hdrip", "hdit",
+                          "high definition"],
+                 "VODRip": ["vodrip", "vodr"],
+                 "WEB-DL": ["webdl", "web dl", "web-dl"],
+                 "WEBRip": ["web-rip", "webrip", "web rip"],
+                 "WEBCap": ["web-cap", "webcap", "web cap"],
+                 "BD/BRRip": ["bdrip", "brrip", "blu-ray", "bluray", "bdr", "bd5", "bd", "blurip"],
+                 "MicroHD": ["microhd"],
+                 "FullHD": ["fullhd"],
+                 "BR-Line": ["br line"],
+                 # video formats
+                 "x264": ["x264", "x 264"],
+                 "x265 HEVC": ["x265 hevc", "x265", "x 265", "hevc"],
+                 # audio
+                 "DD5.1": ["dd5 1", "dd51", "dual audio 5"],
+                 "AC3 5.1": ["ac3"],
+                 "ACC": ["acc"],
+                 "DUAL AUDIO": ["dual", "dual audio"],
+                 }
+    color = {"Cam": "FFF4AE00",
+             "Telesync": "FFF4AE00",
+             "Workprint": "FFF4AE00",
+             "Telecine": "FFF4AE00",
+             "Pay-Per-View Rip": "FFD35400",
+             "Screener": "FFD35400",
+             "DDC": "FFD35400",
+             "R5": "FFD35400",
+             "DVD-Rip": "FFD35400",
+             "DVD-R": "FFD35400",
+             "HDTV": "FFD35400",
+             "VODRip": "FFD35400",
+             "WEB-DL": "FFD35400",
+             "WEBRip": "FFD35400",
+             "WEBCap": "FFD35400",
+             "BD/BRRip": "FFD35400",
+             "MicroHD": "FFD35400",
+             "FullHD": "FFD35400",
+             "BR-Line": "FFD35400",
+             # video formats
+             "x264": "FFFB0C06",
+             "x265 HEVC": "FFFB0C06",
+             # audio
+             "DD5.1": "FF089DE3",
+             "AC3 5.1": "FF089DE3",
+             "ACC": "FF089DE3",
+             "DUAL AUDIO": "FF089DE3",
+             }
+    quality = "480p"
+    text_quality = ""
+    for key in key_words:
+        for keyWord in key_words[key]:
+            if ' ' + keyWord + ' ' in ' ' + text + ' ':
+                quality = "480p"
+                text_quality += " [COLOR %s][%s][/COLOR]" % (color[key], key)
+
+    if "480p" in text:
+        quality = "480p"
+    if "720p" in text:
+        quality = "720p"
+    if "1080p" in text:
+        quality = "1080p"
+    if "3d" in text:
+        quality = "1080p"
+    if "4k" in text:
+        quality = "2160p"
+    return quality
 
 
 # apply sorting based on seeds and quality
@@ -42,48 +118,16 @@ def cleanup_results(results_list):
 def sort_by_quality(results_list):
     logger.log.debug("Applying quality sorting")
     for result in results_list:
-        try:
-            # seeding level
-            if int(result['seeds']) > 200:
-                result['seeding_level'] = 4
-            elif int(result['seeds']) > 150:
-                result['seeding_level'] = 3
-            elif int(result['seeds']) > 100:
-                result['seeding_level'] = 2
-            elif int(result['seeds']) > 50:
-                result['seeding_level'] = 1
-            else:
-                result['seeding_level'] = 0
+        # hd streams
+        quality = check_quality(result['name'])
+        if "1080p" in quality:
+            result['quality'] = 3
+            result['hd'] = 1
+        elif "720p" in quality:
+            result['quality'] = 2
+            result['hd'] = 1
+        else:
+            result['quality'] = 1
+            result['hd'] = 0
 
-            # seeds/peers ratio
-            if float(result['peers']) > 0:
-                seed_ratio = float(result['seeds']) / float(result['peers'])
-            else:
-                seed_ratio = float(result['seeds']) / 1
-
-            if seed_ratio > 2.5:
-                result['seeding_ratio'] = 4
-            elif seed_ratio > 2:
-                result['seeding_ratio'] = 3
-            elif seed_ratio > 1.5:
-                result['seeding_ratio'] = 2
-            elif seed_ratio > 1:
-                result['seeding_ratio'] = 1
-            else:
-                result['seeding_ratio'] = 0
-
-            # hd streams
-            if "1080P" in result['name'].upper():
-                result['quality'] = 3
-                result['hd'] = 1
-            elif "720P" in result['name'].upper():
-                result['quality'] = 2
-                result['hd'] = 1
-            else:
-                result['quality'] = 1
-                result['hd'] = 0
-        except:
-            pass
-
-    return sorted(results_list, key=lambda r: (r['hd'], r['seeding_ratio'],
-                                               r['seeding_level'], r['quality'], r['size'], r['seeds']), reverse=True)
+    return sorted(results_list, key=lambda r: (r["seeds"], r['hd'], r['quality'], r["peers"]), reverse=True)
